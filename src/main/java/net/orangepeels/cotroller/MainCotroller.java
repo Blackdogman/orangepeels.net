c7b4d73cfbdfcab3bb8a30385b6f761432f392e4
Blackdogman <superkidchy@qq.com>
2018-05-31 14:47:25 +0800

    更新了针对小屏幕设备的兼容

--- src/main/java/net/orangepeels/cotroller/MainCotroller.java
+++ src/main/java/net/orangepeels/cotroller/MainCotroller.java
@@ -2,14 +2,29 @@ package net.orangepeels.cotroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainCotroller {
 
     @GetMapping("/")
-    public String WelcomeToWebsite(){
+    public String WelcomeToWebsite(HttpServletRequest req){
         return "homePage";
     }
 
+    @RequestMapping("/test")
+    @ResponseBody
+    public List<String> getJSON(HttpServletRequest req){
+        List<String> list = new ArrayList<>();
+        list.add(req.getParameter("user_name"));
+        list.add(req.getParameter("user_pwd"));
+        return list;
+    }
 
 }
