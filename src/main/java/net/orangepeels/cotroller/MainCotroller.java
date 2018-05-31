81871c2f98fad8e053f9adfc55a935fd763cdb91
Blackdogman <superkidchy@qq.com>
2018-05-31 10:07:51 +0800

    准备引入mybatis准备

--- src/main/java/net/orangepeels/cotroller/MainCotroller.java
+++ src/main/java/net/orangepeels/cotroller/MainCotroller.java
@@ -1,12 +1,15 @@
 package net.orangepeels.cotroller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainCotroller {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String WelcomeToWebsite(){
         return "homePage";
     }
+
+
 }
