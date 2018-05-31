f52f5b5579522f4309d74a94376519abdc120d52
Blackdogman <superkidchy@qq.com>
2018-05-31 11:49:49 +0800

    修正了配置文件的路径问题

--- src/main/java/net/orangepeels/cotroller/MainCotroller.java
+++ src/main/java/net/orangepeels/cotroller/MainCotroller.java
@@ -8,12 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainCotroller {
-    @Autowired
-    private UserDao userDao;
 
     @GetMapping("/")
     public String WelcomeToWebsite(){
-        System.out.println(userDao.selectId(1));
         return "homePage";
     }
 
