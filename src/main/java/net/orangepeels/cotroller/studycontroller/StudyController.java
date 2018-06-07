6a8eb95b997290943ebeeda4d1e615b5927a6213
Blackdogman <superkidchy@qq.com>
2018-06-07 10:39:38 +0800

    update MarkDownTools(能做一层的#标签与-标签了，接下来```标签)

---
+++ src/main/java/net/orangepeels/cotroller/studycontroller/StudyController.java
@@ -0,0 +1,40 @@
+package net.orangepeels.cotroller.studycontroller;
+
+import net.orangepeels.utils.MarkDown.MarkDownTools;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class StudyController {
+
+    /**
+     * 文件上传
+     * @param req
+     * @// TODO: 2018/6/7 并没有完成，不知道该怎么做
+     */
+    @RequestMapping("/upFile")
+    public void upFile(HttpServletRequest req){
+        String file = req.getParameter("file");
+        System.out.println("file: " + file);
+    }
+
+    /**
+     * 把markdown转换成HTML代码返回
+     * @return 返回一个HTML代码
+     */
+    @RequestMapping("/getHTMLFromMD")
+    public String getHTMLFromMD(){
+        String reStr = "";
+        File file = new File("/Users/Blackdogman/Desktop/markdown.md");
+        try {
+            reStr = MarkDownTools.getHTML(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return reStr;
+    }
+}
--- src/main/java/net/orangepeels/utils/MarkDown/MarkDownTools.java
+++ src/main/java/net/orangepeels/utils/MarkDown/MarkDownTools.java
@@ -16,7 +16,7 @@ public class MarkDownTools {
         }
     }
 
-    public String getHTML(File file) throws IOException {
+    public static String getHTML(File file) throws IOException {
         String strHTML = "";
         strHTML = getFistLab(file);
         return strHTML;
@@ -28,39 +28,66 @@ public class MarkDownTools {
      * @return
      * @throws IOException
      */
-    private String getFistLab(File file) throws IOException {
-        String toLab = "";
+    private static String getFistLab(File file) throws IOException {
+        StringBuilder toLab = new StringBuilder();
+        String tag = "";
+        boolean ulFlag = false;
+        String codeFlag = "";
         InputStream input = new FileInputStream(file);
         InputStreamReader reader = new InputStreamReader(input);
-        BufferedReader br = new BufferedReader(reader);
-        String temp = "";
+        BufferedReader br = new BufferedReader(reader); // markdown读取器
+        String temp;
         while ((temp = br.readLine()) != null){
-            toLab += getFistLabTab(temp) + "\r\n";
+            tag = temp.substring(0, 1);
+            if("-".equals(tag) && ulFlag== false){
+                toLab.append("<ul>\r\n");
+                ulFlag = true;
+            }
+            if(!"-".equals(tag) && ulFlag == true){
+                toLab.append("</ul>\r\n");
+                ulFlag = false;
+            }
+            switch (tag){
+                case "#":
+                    toLab.append(hTag(temp));
+                    break;
+                case "-":
+                    toLab.append(liTag(temp));
+                    break;
+            }
         }
         System.out.println(toLab);
-        return toLab;
+        return toLab.toString();
     }
 
-    /**
-     * 逐行处理markdown
-     * @param temp 每行的文本
-     * @return 对应的HTML文本
-     */
-    private String getFistLabTab(String temp){
-        String tab = "";
-        String tag = temp.substring(0, 1);
-        if("#".equals(tag)){
-            int hNum = temp.indexOf(" "); //看有几个井号
-            String start,end;
-            start = "<h"+ hNum +">";
-            end = "</h"+ hNum +">";
-            tab += start + "\r\n";
-            tab += temp.substring(temp.indexOf(" "), temp.length());
-            tab += "\r\n" + end;
-            System.out.println(tab);
-        }else {
-            tab = temp;
-        }
+    //对应无序列表
+    private static String liTag(String temp){
+        String tab = "";  // 组合的标签
+        String start,end; // 记录标签头与标签尾
+        start = "<li>";
+        end = "</li>";
+        tab += start;
+        tab += "\r\n";
+        tab += temp.substring(temp.indexOf(" "), temp.length());
+        tab += "\r\n";
+        tab += end;
+        tab += "\r\n";
+        return tab;
+    }
+
+    //生成对应的h1~h6标题
+    private static String hTag(String temp){
+        String tab = "";  // 组合的标签
+        String start,end; // 记录标签头与标签尾
+        int hNum = temp.indexOf(" "); //看有几个井号
+        start = "<h"+ hNum +">";
+        end = "</h"+ hNum +">";
+        tab += start;
+        tab += "\r\n";
+        tab += temp.substring(temp.indexOf(" "), temp.length());
+        tab += "\r\n";
+        tab += end;
+        tab += "\r\n";
         return tab;
     }
 }
