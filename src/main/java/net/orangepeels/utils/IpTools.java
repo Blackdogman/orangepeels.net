22ca6473c1a9cb8ef1c1ff213d9dec2073b4ec01
Blackdogman <superkidchy@qq.com>
2018-07-12 15:48:24 +0800

    增加了geoip2来解析IP地址

---
+++ src/main/java/net/orangepeels/utils/IpTools.java
@@ -0,0 +1,68 @@
+package net.orangepeels.utils;
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.record.City;
+import com.maxmind.geoip2.record.Country;
+import com.maxmind.geoip2.record.Subdivision;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class IpTools {
+    private String countryName;
+    private String countryNameZhCN;
+    private String subdivisionName;
+    private String subdivisionNameZhCN;
+    private String cityName;
+    private String cityNameZhCN;
+
+    public void resolveIp(String ip) throws IOException, GeoIp2Exception {
+        // 得到地区数据库文件
+        File database = ResourceUtils.getFile("classpath:common/GeoLite2-City.mmdb");
+        // 读取数据库内容
+        DatabaseReader reader = new DatabaseReader.Builder(database).build();
+        InetAddress ipAddress = InetAddress.getByName(ip);
+        // 获取查询结果
+        CityResponse response = reader.city(ipAddress);
+        // 得到国家
+        Country country = response.getCountry();
+        this.countryName = country.getName();
+        this.countryNameZhCN = country.getNames().get("zh-CN");
+        // 获得省份
+        Subdivision subdivision = response.getMostSpecificSubdivision();
+        this.subdivisionName = subdivision.getName();
+        this.subdivisionNameZhCN = subdivision.getNames().get("zh-CH");
+        // 获得城市
+        City city = response.getCity();
+        this.cityName = city.getName();
+        this.cityNameZhCN = city.getNames().get("zh-CH");
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCountryNameZhCN() {
+        return countryNameZhCN;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCityNameZhCN() {
+        return cityNameZhCN;
+    }
+
+    public String getSubdivisionName() {
+        return subdivisionName;
+    }
+
+    public String getSubdivisionNameZhCN() {
+        return subdivisionNameZhCN;
+    }
+}
