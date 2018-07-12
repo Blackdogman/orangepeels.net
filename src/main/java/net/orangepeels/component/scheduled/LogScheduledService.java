aecec31aaf47c3954a4adf23bde30181d4885c48
Blackdogman <superkidchy@qq.com>
2018-07-12 16:56:31 +0800

    修正了下逻辑，保证安全

--- src/main/java/net/orangepeels/component/scheduled/LogScheduledService.java
+++ src/main/java/net/orangepeels/component/scheduled/LogScheduledService.java
@@ -13,16 +13,18 @@ public class LogScheduledService {
     private final static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @Scheduled(cron = "1 59 23 1/1 * ?")
-    public void sendLogMailEveryDay(){
+    public void sendLogMailEveryDay() {
         String path = System.getProperty("user.dir");
         String yMd = simpleDateFormat.format(new Date());
         // 访问日志文件，作为附件发送
-        File file = new File(path + "/log-Orangepeels/log"+yMd+".txt");
+        File file = new File(path + "/log-Orangepeels/log" + yMd + ".txt");
         MailSender mailSender = new MailSender();
         mailSender.setToAddress("superkidchy@foxmail.com");
-        mailSender.setSubject("OrangePeels-" + simpleDateFormat.format(new Date()) + "-visitLog");
+        mailSender.setSubject("OrangePeels-" + yMd + "-visitLog");
         mailSender.setContent("N/A");
-        mailSender.setMailFile(file);
+        if(file.exists()){
+            mailSender.setMailFile(file);
+        }
         mailSender.send();
     }
 }
