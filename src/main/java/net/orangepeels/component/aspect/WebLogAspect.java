cb208990f383cb69ee9e13d723a0532a5dd004c5
Blackdogman <superkidchy@qq.com>
2018-07-12 15:59:35 +0800

    在记录访问的时候解析ip所在地

--- src/main/java/net/orangepeels/component/aspect/WebLogAspect.java
+++ src/main/java/net/orangepeels/component/aspect/WebLogAspect.java
@@ -1,5 +1,7 @@
 package net.orangepeels.component.aspect;
 
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import net.orangepeels.utils.IpTools;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -23,31 +25,42 @@ public class WebLogAspect {
     }
 
     @Before("webLog()")
-    public void beforeWebLog() {
+    public void beforeWebLog() throws IOException, GeoIp2Exception {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
+        String reqIpAddress = request.getRemoteAddr();
+        boolean localIpFlag = false;
+        if ("0:0:0:0:0:0:0:1".equals(reqIpAddress) || "localhost".equals(reqIpAddress) || "127.0.0.1".equals(reqIpAddress)) {
+            localIpFlag = true;
+        }
         StringBuilder sb = new StringBuilder();
         sb.append("访问时间: ").append(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()));
         sb.append("\r\n");
-        sb.append("客户端IP: " + request.getRemoteAddr());
+        sb.append("客户端IP: " + reqIpAddress);
         sb.append("\r\n");
+        if (!localIpFlag) { // 如果为本地地址，那么就不解析ip所在地
+            IpTools ipInfo = new IpTools();
+            ipInfo.resolveIp(reqIpAddress);
+            sb.append("ip所在地: " + ipInfo.getCountryName() + " " + ipInfo.getSubdivisionName() + " " + ipInfo.getCityName());
+            sb.append("\r\n");
+        }
         sb.append("访问地址: " + request.getRequestURI());
         sb.append("\r\n\r\n");
         String path = System.getProperty("user.dir");
         //得到项目发布路径
         //String path = Class.class.getClass().getResource("/").getPath();
         String yMd = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        File file = new File(path + "/log-Orangepeels/log"+yMd+".txt");
-        if(!file.getParentFile().getParentFile().getParentFile().exists()){
+        File file = new File(path + "/log-Orangepeels/log" + yMd + ".txt");
+        if (!file.getParentFile().getParentFile().getParentFile().exists()) {
             file.getParentFile().getParentFile().getParentFile().mkdir();
         }
-        if(!file.getParentFile().getParentFile().exists()){
+        if (!file.getParentFile().getParentFile().exists()) {
             file.getParentFile().getParentFile().mkdir();
         }
         if (!file.getParentFile().exists()) {
             file.getParentFile().mkdir();
         }
-        if(!file.exists()){
+        if (!file.exists()) {
             try {
                 file.createNewFile();
             } catch (IOException e) {
