f1b7fc603127ca1f0d29a5cb79bc77b54b9178e6
Blackdogman <superkidchy@foxmail.com>
2018-07-25 20:17:44 +0800

    game初步功能完成，等待加强

--- src/main/java/net/orangepeels/cotroller/GameController.java
+++ src/main/java/net/orangepeels/cotroller/GameController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @RestController
@@ -19,9 +20,24 @@ public class GameController {
     }
 
     @RequestMapping("/getValCode.do")
-    public void getValCode(HttpServletResponse response) throws IOException {
+    public void getValCode(HttpServletResponse response, HttpSession session) throws IOException {
         valCode.createCode();
+        session.setAttribute("gameValCode", valCode.getCode());
         response.setContentType("img/png");
         valCode.write(response.getOutputStream());
+    }
+
+    @RequestMapping("/submitGameCode.do")
+    public int submitGameCode(String number1, String number2, String number3, String number4, HttpSession session) {
+        String gameValCode = (String) session.getAttribute("gameValCode");
+        String answer = number1 +
+                number2 +
+                number3 +
+                number4;
+        if(gameValCode.equalsIgnoreCase(answer)){
+            return 1;
+        }else{
+            return 0;
+        }
     }
 }
