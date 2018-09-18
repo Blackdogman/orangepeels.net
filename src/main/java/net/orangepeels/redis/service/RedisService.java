71742bf92d5494d3aabfbbdd5b2d106bf68c8869
Blackdogman <superkidchy@foxmail.com>
2018-09-18 11:22:18 +0800

    添加了下注释，优化代码

--- src/main/java/net/orangepeels/redis/service/RedisService.java
+++ src/main/java/net/orangepeels/redis/service/RedisService.java
@@ -4,10 +4,21 @@ import java.util.Map;
 import java.util.Set;
 
 public interface RedisService {
-//    得到redis中所有的key
+    /**
+     * 得到redis中所有的key
+     * @return 装有redis中所有key的Set
+     */
     Set<String> getAllKeys();
-//    得到对应key的value
+    /**
+     * 得到redis中对应的key的值
+     * @param key redis中的key值
+     * @return 返回一个{key: value}的Map对象
+     */
     Map<String, Object> getValue(String key);
-//    得到对应key的type
+    /**
+     * 得到对应key的type
+     * @param key redis中的key值
+     * @return 对应key值得对象类型(string, list, hash....)
+     */
     String getType(String key);
 }
