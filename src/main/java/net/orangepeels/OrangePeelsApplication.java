f0b25c676401109ec630022c0d1e6892170e4c05
Blackdogman <superkidchy@foxmail.com>
2018-09-18 10:58:44 +0800

    单独把config bean注册提取出来，保证启动类的整洁。

--- src/main/java/net/orangepeels/OrangePeelsApplication.java
+++ src/main/java/net/orangepeels/OrangePeelsApplication.java
@@ -1,19 +1,12 @@
 package net.orangepeels;
 
-import com.google.code.kaptcha.Constants;
-import com.google.code.kaptcha.impl.DefaultKaptcha;
-import com.google.code.kaptcha.util.Config;
 import net.orangepeels.component.interceptor.NothingInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import redis.clients.jedis.Jedis;
-
-import java.util.Properties;
 
 
 @SpringBootApplication
@@ -27,30 +20,6 @@ public class OrangePeelsApplication implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new NothingInterceptor()).addPathPatterns("/ICantDoAnything");
-    }
-
-    @Bean
-    public DefaultKaptcha captchaProducer() {
-        DefaultKaptcha captchaProducer = new DefaultKaptcha();
-        Properties properties = new Properties();
-        properties.setProperty(Constants.KAPTCHA_BORDER, "yes");
-        properties.setProperty(Constants.KAPTCHA_BORDER_COLOR, "red");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_COLOR, "blue");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_SIZE, "50");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_NAMES, "宋体,楷体,微软雅黑");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_LENGTH, "4");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_STRING, "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ");
-        properties.setProperty(Constants.KAPTCHA_IMAGE_WIDTH, "200");
-        properties.setProperty(Constants.KAPTCHA_IMAGE_HEIGHT, "50");
-        properties.setProperty(Constants.KAPTCHA_SESSION_CONFIG_KEY, "code");
-        Config config = new Config(properties);
-        captchaProducer.setConfig(config);
-        return captchaProducer;
-    }
-
-    @Bean
-    public Jedis jedis(){
-        return new Jedis("127.0.0.1");
     }
 }
 
